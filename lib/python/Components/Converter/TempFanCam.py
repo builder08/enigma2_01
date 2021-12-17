ba3662c157299063a127795e06d9becc6ce0ca1f
builder08 <75539906+builder08@users.noreply.github.com>
2021-12-17 22:35:47 +0200

    Update TempFanCam.py

--- lib/python/Components/Converter/TempFanCam.py
+++ lib/python/Components/Converter/TempFanCam.py
@@ -1,4 +1,4 @@
-# TempFanCam (c) BlackFish 2020
+# TempFanCam (c) BlackFish 2021
 # -*- coding: utf-8 -*-
 from Components.Converter.Converter import Converter
 from Components.Sensors import sensors
