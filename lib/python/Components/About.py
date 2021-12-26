06cd7cfbe14705d0df883aff62ff8adf7ee7e762
builder08 <75539906+builder08@users.noreply.github.com>
2021-12-26 22:10:50 +0200

    Add files via upload

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -1,13 +1,16 @@
 from __future__ import print_function
+import sys, os, time
+import re
 from sys import modules, version as pyversion
 from fcntl import ioctl
 from struct import pack
 from socket import socket, inet_ntoa, AF_INET, SOCK_DGRAM
 from time import localtime, strftime
 from os import stat
-
+from Components.SystemInfo import SystemInfo
 from boxbranding import getBoxType, getMachineBuild, getImageVersion
 from Tools.Directories import fileReadLine, fileReadLines
+from subprocess import PIPE, Popen
 
 MODULE_NAME = __name__.split(".")[-1]
 
@@ -21,23 +24,61 @@ def getVersionString():
 
 def getFlashDateString():
 	try:
-		tm = localtime(stat("/boot").st_ctime)
-		if tm.tm_year >= 2011:
-			return strftime(_("%Y-%m-%d"), tm)
-		else:
-			return _("unknown")
+		# return time.strftime(_("%Y-%m-%d %H:%M:%S"), time.localtime(os.stat("/etc/version").st_ctime))
+		return time.strftime(_("%Y-%m-%d %H:%M:%S"), time.localtime(os.path.getatime("/bin")))
 	except:
 		return _("unknown")
 
-
 def getEnigmaVersionString():
-	return getImageVersion()
-
+	import enigma
+	enigma_version = enigma.getEnigmaVersionString()
+	if '-(no branch)' in enigma_version:
+		enigma_version = enigma_version [:-12]
+	return enigma_version	
 
 def getGStreamerVersionString():
-	from enigma import getGStreamerVersionString
-	return getGStreamerVersionString()
+	try:
+		from glob import glob
+		gst = [x.split("Version: ") for x in open(glob("/var/lib/opkg/info/gstreamer[0-9].[0-9].control")[0], "r") if x.startswith("Version:")][0]
+		return "%s" % gst[1].split("+")[0].replace("\n","")
+	except:
+		return _("Not Required") if cpu.upper().startswith('HI') else _("Not Installed")
 
+def getFFmpegVersionString():
+	try:
+		from glob import glob
+		ffmpeg = [x.split("Version: ") for x in open(glob("/var/lib/opkg/info/ffmpeg.control")[0], "r") if x.startswith("Version:")][0]
+		version = ffmpeg[1].split("-")[0].replace("\n", "")
+		return "%s" % version.split("+")[0]
+	except:
+		return _("unknown")
+
+def getGlibcVersion():
+	process = Popen(("/lib/libc.so.6"), stdout=PIPE, stderr=PIPE, universal_newlines=True)
+	stdout, stderr = process.communicate()
+	if process.returncode == 0:
+		for line in stdout.split("\n"):
+			if line.startswith("GNU C Library"):
+				data = line.split()[-1]
+				if data.endswith("."):
+					data = data[0:-1]
+				return data
+	print("[About] Get glibc version failed.")
+	return _("Unknown")
+
+
+def getGccVersion():
+	process = Popen(("/lib/libc.so.6"), stdout=PIPE, stderr=PIPE, universal_newlines=True)
+	stdout, stderr = process.communicate()
+	if process.returncode == 0:
+		for line in stdout.split("\n"):
+			if line.startswith("Compiled by GNU CC version"):
+				data = line.split()[-1]
+				if data.endswith("."):
+					data = data[0:-1]
+				return data
+	print("[About] Get gcc version failed.")
+	return _("Unknown")
 
 def getKernelVersionString():
 	try:
@@ -48,6 +89,11 @@ def getKernelVersionString():
 	except:
 		return _("unknown")
 
+def getDVBAPI():
+	if SystemInfo["OLDE2API"]:
+		return _("Old")
+	else:
+		return _("New")
 
 def getModelString():
 	model = getBoxType()
@@ -122,6 +168,21 @@ def getCPUSpeedString():
 		except IOError:
 			return "unavailable"
 
+def getImageTypeString():
+	try:
+		image_type = open("/etc/issue").readlines()[-2].strip("openfix-")[:-6]
+		return image_type.capitalize()
+	except:
+		return _("undefined")	
+
+def getBuildDateString():
+	try:
+		if os.path.isfile('/etc/version'):
+			version = open("/etc/version","r").read()
+			return "%s-%s-%s" % (version[:4], version[4:6], version[6:8])
+	except:
+		pass
+	return _("unknown")
 
 def getCPUString():
 	if getMachineBuild() in ('vuduo4k', 'vuduo4kse', 'osmio4k', 'osmio4kplus', 'osmini4k', 'dags72604', 'vuuno4kse', 'vuuno4k', 'vuultimo4k', 'vusolo4k', 'vuzero4k', 'hd51', 'hd52', 'sf4008', 'dm900', 'dm920', 'gb7252', 'gb72604', 'dags7252', 'vs1500', 'et1x000', 'xc7439', 'h7', '8100s', 'et13000', 'sf5008'):
@@ -148,6 +209,75 @@ def getCPUString():
 		except IOError:
 			return "unavailable"
 
+def getCPUSerial():
+	with open('/proc/cpuinfo', 'r') as f:
+		for line in f:
+			if line[0:6] == 'Serial':
+				return line[10:26]
+		return "0000000000000000"
+		
+def getCPUArch():
+	if SystemInfo["ArchIsARM64"]:
+		return _("ARM64")
+	elif SystemInfo["ArchIsARM"]:
+		return _("ARM")
+	else:
+		return _("Mipsel")	
+		
+def getCPUInfoString():
+	try:
+		cpu_count = 0
+		cpu_speed = 0
+		processor = ""
+		for line in open("/proc/cpuinfo").readlines():
+			line = [x.strip() for x in line.strip().split(":")]
+			if not processor and line[0] in ("system type", "model name", "Processor"):
+				processor = line[1].split()[0]
+			elif not cpu_speed and line[0] == "cpu MHz":
+				cpu_speed = "%1.0f" % float(line[1])
+			elif line[0] == "processor":
+				cpu_count += 1
+
+		if processor.startswith("ARM") and os.path.isfile("/proc/stb/info/chipset"):
+			processor = "%s (%s)" % (open("/proc/stb/info/chipset").readline().strip().upper(), processor)
+
+		if not cpu_speed:
+			try:
+				cpu_speed = int(open("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq").read()) / 1000
+			except:
+				try:
+					import binascii
+					cpu_speed = int(int(binascii.hexlify(open('/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency', 'rb').read()), 16) / 100000000) * 100
+				except:
+					cpu_speed = "-"
+
+		temperature = None
+		if os.path.isfile('/proc/stb/fp/temp_sensor_avs'):
+			temperature = open("/proc/stb/fp/temp_sensor_avs").readline().replace('\n', '')
+		elif os.path.isfile('/proc/stb/power/avs'):
+			temperature = open("/proc/stb/power/avs").readline().replace('\n', '')
+		elif os.path.isfile('/proc/stb/fp/temp_sensor'):
+			temperature = open("/proc/stb/fp/temp_sensor").readline().replace('\n', '')
+		elif os.path.isfile('/proc/stb/sensors/temp0/value'):
+			temperature = open("/proc/stb/sensors/temp0/value").readline().replace('\n', '')
+		elif os.path.isfile('/proc/stb/sensors/temp/value'):
+			temperature = open("/proc/stb/sensors/temp/value").readline().replace('\n', '')
+		elif os.path.isfile("/sys/devices/virtual/thermal/thermal_zone0/temp"):
+			try:
+				temperature = int(open("/sys/devices/virtual/thermal/thermal_zone0/temp").read().strip()) / 1000
+			except:
+				pass
+		elif os.path.isfile("/proc/hisi/msp/pm_cpu"):
+			try:
+				temperature = re.search('temperature = (\d+) degree', open("/proc/hisi/msp/pm_cpu").read()).group(1)
+			except:
+				pass
+		if temperature:
+			# return "%s %s MHz (%s) %s\xb0C" % (processor, cpu_speed, ngettext("%d core", "%d cores", cpu_count) % cpu_count, temperature)
+			return "%s\xb0C" % (temperature)
+		# return "%s %s MHz (%s)" % (processor, cpu_speed, ngettext("%d core", "%d cores", cpu_count) % cpu_count)
+	except:
+		return _("undefined")			
 
 def getCpuCoresString():
 	try:
@@ -233,6 +363,20 @@ def getBoxUptime():
 	times.append(ngettext("%d minute", "%d minutes", m) % m)
 	return " ".join(times)
 
+def getIdea():
+	return _("BlackFish")
+
+def getEmail():
+	return _("blackfish.3654@gmail.com")
+		
+def getBrand():
+	return _("octagon")			
+
+def getDonate():
+	return _("Z541154775569, R610636086219")
+		
+def getThanks():
+	return _("gisclub.tv, opena.tv, ShadowA, moskvish")
 
 def getopensslVersionString():
 	lines = fileReadLines("/var/lib/opkg/info/openssl.control", source=MODULE_NAME)
