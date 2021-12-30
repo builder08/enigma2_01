ffb4a63100f4d1cf02ec5a4a62ddd9df8a92a5a1
builder08 <75539906+builder08@users.noreply.github.com>
2021-12-30 11:24:16 +0200

    Update MessageBox.py

--- lib/python/Screens/MessageBox.py
+++ lib/python/Screens/MessageBox.py
@@ -268,3 +268,9 @@ class MessageBox(Screen, HelpableScreen):
 
 	def __repr__(self):
 		return "%s(%s)" % (str(type(self)), self.text)
+
+		def getListWidth(self):
+		def getListLineTextWidth(text):
+			self["autoresize"].setText(text)
+			return self["autoresize"].getSize()[0]
+		return max([getListLineTextWidth(line[0]) for line in self.list]) if self.list else 0
